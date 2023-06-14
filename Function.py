05391a75160f159d6e4eabbcd338cb13bb16c155
Miad Bhuiyan <124463760+miadbhuiyan@users.noreply.github.com>
2023-06-14 09:43:49 -0500

    Add files via upload

---
+++ Function.py
@@ -0,0 +1,29 @@
+def sum(a=10,b=29):
+    print(a+b)
+    print("Hi Miad ")
+sum()
+
+def add_num(num1,num2):
+    sum = num1 + num2
+    print(sum)
+add_num(10,1121)
+
+
+def find_square(number):
+    result = number * number
+    return result
+square = find_square(3)
+print('Square',square)
+
+
+import math
+
+# sqrt computes the square root
+square_root = math.sqrt(4)
+
+print("Square Root of 4 is",square_root)
+
+# pow() comptes the power
+power = pow(2, 3)
+
+print("2 to the power 3 is",power)
